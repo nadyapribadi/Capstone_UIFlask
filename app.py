2436d0e2b25ff3d996119b86eda47e657f8cd059
nadyapribadi <nadya.pribadi@gmail.com>
2020-08-08 20:32:40 +0700

    Checkpoint Day 3

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ import base64
 app = Flask(__name__)
 
 # bacalah file `googleplaystore.csv` data dan simpan ke objek dataframe dengan nama playstore
-playstore =  pd.read_csv("/data/googleplaystore.csv")
+playstore = pd.read_csv("data\googleplaystore.csv")
 
 # Hapus data yang duplikat berdasarkan kolom App, dengan tetap keep data pertama (hint : gunakan parameter subset)
-playstore.drop_duplicates(subset = ['App'], keep ="first")
+playstore.drop_duplicates(subset = ['App'], keep ="first", inplace = True)
 
 # bagian ini untuk menghapus row 10472 karena nilai data tersebut tidak tersimpan pada kolom yang benar
 playstore.drop([10472], inplace=True)
@@ -20,10 +20,10 @@ playstore.drop([10472], inplace=True)
 playstore['Category'] = playstore['Category'].astype('category')
 
 # Pada kolom Installs Buang tanda koma(,) dan tanda tambah(+) kemudian ubah tipe data menjadi integer
-playstore['Installs'] = playstore['Installs'].apply(lambda x: x.replace(',',''))
-playstore['Installs'] = playstore['Installs'].apply(lambda x: x.replace('+',''))
+playstore.Installs = playstore.Installs.apply(lambda x: x.replace(',',''))
+playstore.Installs = playstore.Installs.apply(lambda x: x.replace('+',''))
 # bagian untuk mengubah tipe data Installs
-playstore['Installs'] = playstore['Installs'].astype('int64')
+playstore.Installs = playstore.Installs.astype('int64')
 
 # Bagian ini untuk merapikan kolom Size, Anda tidak perlu mengubah apapun di bagian ini
 playstore['Size'].replace('Varies with device', np.nan, inplace = True ) 
@@ -34,11 +34,12 @@ playstore.Size = (playstore.Size.replace(r'[kM]+$', '', regex=True).astype(float
 playstore['Size'].fillna(playstore.groupby('Category')['Size'].transform('mean'),inplace = True)
 
 # Pada kolom Price, buang karakater $ pada nilai Price lalu ubah tipe datanya menjadi float
-playstore['Price'] = playstore['Price'].apply(lambda x: x.replace('$',''))
-playstore['Price'] = playstore['Price'].astype('float64')
+playstore.Price = playstore.Price.apply(lambda x: x.replace('$',''))
+playstore.Price = playstore.Price.astype('float64')
 
 # Ubah tipe data Reviews, Size, Installs ke dalam tipe data integer
 playstore[['Reviews','Size','Installs']] = playstore[['Reviews','Size','Installs']].astype('int64')
+
 
 @app.route("/")
 # This fuction for rendering the table
@@ -49,61 +50,67 @@ def index():
     # Dataframe top_category dibuat untuk menyimpan frekuensi aplikasi untuk setiap Category. 
     # Gunakan crosstab untuk menghitung frekuensi aplikasi di setiap category kemudian gunakan 'Jumlah'
     # sebagai nama kolom dan urutkan nilai frekuensi dari nilai yang paling banyak. Terakhir reset index dari dataframe top_category 
-    top_category = ______________________________
+    top_category = pd.crosstab(index=df2['Category'],
+                           columns="Jumlah",
+                           values=df2['App'],
+                           aggfunc='count').sort_values('Jumlah', ascending = False).reset_index()
     # Dictionary stats digunakan untuk menyimpan beberapa data yang digunakan untuk menampilkan nilai di value box dan tabel
     stats = {
         # Ini adalah bagian untuk melengkapi konten value box 
         # most category mengambil nama category paling banyak mengacu pada dataframe top_category
         # total mengambil frekuensi/jumlah category paling banyak mengacu pada dataframe top_category
-        'most_categories' : __________,
-        'total': ____________,
-        # rev_table adalah tabel yang berisi 10 aplikasi yang paling banyak direview oleh pengguna. 
-        # Silahkan melakukan agregasi data yang tepat menggunakan groupby untuk menampilkan 10 aplikasi yang diurutkan berdasarkan 
+        'most_categories': top_category['Category'].loc[0],
+        'total': top_category['Jumlah'].loc[0],
+        # rev_table adalah tabel yang berisi 10 aplikasi yang paling banyak direview oleh pengguna.
+        # Silahkan melakukan agregasi data yang tepat menggunakan groupby untuk menampilkan 10 aplikasi yang diurutkan berdasarkan
         # jumlah Review pengguna. Tabel yang ditampilkan terdiri dari 4 kolom yaitu nama Category, nama App, total Reviews, dan rata-rata Rating.
         # Agregasi Anda dinilai benar jika hasilnya sama dengan tabel yang terlampir pada file ini
-        'rev_table' : ___________________.to_html(classes=['table thead-light table-striped table-bordered table-hover table-sm'])
+        'rev_table': df2[["Category","App","Reviews","Rating"]].groupby(["Category","App"]).\
+         agg({'Reviews':'sum', 'Rating' : 'mean'}).\
+         sort_values(["Reviews","Category"], ascending = False).\
+         groupby(["Category","Reviews"]).\
+         head(5).reset_index().head(10).to_html(
+            classes=['table thead-light table-striped table-bordered table-hover table-sm'])
     }
 
     ## Bar Plot
     ## Lengkapi tahap agregasi untuk membuat dataframe yang mengelompokkan aplikasi berdasarkan Category
     ## Buatlah bar plot dimana axis x adalah nama Category dan axis y adalah jumlah aplikasi pada setiap kategori, kemudian urutkan dari jumlah terbanyak
-    cat_order = df2.groupby(_______).agg({
-    _________ : _________
-        }).rename({'Category':'Total'}, axis=1).sort_values(__________).head()
-    X = _____________
-    Y = _____________
+    cat_order = df2.groupby(['Category']).agg({'App' : 'count'},axis=1).rename({'App': 'Total'}, axis=1).sort_values('Total', ascending = False).head().reset_index()
+    X = cat_order['Category']
+    Y = cat_order['Total']
     my_colors = 'rgbkymc'
     # bagian ini digunakan untuk membuat kanvas/figure
-    fig = plt.figure(figsize=(8,3),dpi=300)
+    fig = plt.figure(figsize=(8, 3), dpi=300)
     fig.add_subplot()
     # bagian ini digunakan untuk membuat bar plot
     # isi variabel x dan y yang telah di definisikan di atas
-    plt.barh(____,_____, color=my_colors)
+    plt.barh(X, Y, color=my_colors,)
     # bagian ini digunakan untuk menyimpan plot dalam format image.png
-    plt.savefig('cat_order.png',bbox_inches="tight") 
+    plt.savefig('cat_order.png', bbox_inches="tight")
 
     # bagian ini digunakan untuk mengconvert matplotlib png ke base64 agar dapat ditampilkan ke template html
     figfile = BytesIO()
     plt.savefig(figfile, format='png')
     figfile.seek(0)
     figdata_png = base64.b64encode(figfile.getvalue())
-    # variabel result akan dimasukkan ke dalam parameter di fungsi render_template() agar dapat ditampilkan di 
+    # variabel result akan dimasukkan ke dalam parameter di fungsi render_template() agar dapat ditampilkan di
     # halaman html
     result = str(figdata_png)[2:-1]
-    
+
     ## Scatter Plot
     # Buatlah scatter plot untuk menampilkan hubungan dan persebaran apalikasi dilihat dari Review vs Rating.
-    # Ukuran scatter menggambarkan berapa banyak pengguna yang telah menginstall aplikasi 
-    X = df2[______].values # axis x
-    Y = df2[______].values # axis y
-    area = playstore[_______].values/10000000 # ukuran besar/kecilnya lingkaran scatter plot
-    fig = plt.figure(figsize=(5,5))
+    # Ukuran scatter menggambarkan berapa banyak pengguna yang telah menginstall aplikasi
+    X = df2['Reviews'].values  # axis x
+    Y = df2['Rating'].values  # axis y
+    area = df2['Installs'].values / 10000000  # ukuran besar/kecilnya lingkaran scatter plot
+    fig = plt.figure(figsize=(5, 5))
     fig.add_subplot()
     # isi nama method untuk scatter plot, variabel x, dan variabel y
-    plt._______(x=_____,y=______, s=area, alpha=0.3)
+    plt.scatter(x=X, y=Y, s=area, alpha=0.3)
     plt.xlabel('Reviews')
     plt.ylabel('Rating')
-    plt.savefig('rev_rat.png',bbox_inches="tight")
+    plt.savefig('rev_rat.png', bbox_inches="tight")
 
     figfile = BytesIO()
     plt.savefig(figfile, format='png')
@@ -112,15 +119,15 @@ def index():
     result2 = str(figdata_png)[2:-1]
 
     ## Histogram Size Distribution
-    # Buatlah sebuah histogram yang menggambarkan distribusi Size aplikasi dalam satuan Mb(Megabytes) 
+    # Buatlah sebuah histogram yang menggambarkan distribusi Size aplikasi dalam satuan Mb(Megabytes)
     # Histogram yang terbentuk terbagi menjadi 100 bins
-    X=(___________/1000000).values
-    fig = plt.figure(figsize=(5,5))
+    X = (df2['Size'] / 1000000).values
+    fig = plt.figure(figsize=(5, 5))
     fig.add_subplot()
-    plt._______(_____,bins=100, density=True,  alpha=0.75)
+    plt.hist(X, bins=100, density=True, alpha=0.75)
     plt.xlabel('Size')
     plt.ylabel('Frequency')
-    plt.savefig('hist_size.png',bbox_inches="tight")
+    plt.savefig('hist_size.png', bbox_inches="tight")
 
     figfile = BytesIO()
     plt.savefig(figfile, format='png')
@@ -128,13 +135,30 @@ def index():
     figdata_png = base64.b64encode(figfile.getvalue())
     result3 = str(figdata_png)[2:-1]
 
-    ## Buatlah sebuah plot yang menampilkan insight di dalam data 
-    ____________________________
-    ____________________________
-    ____________________________
+    ## Buatlah sebuah plot yang menampilkan insight di dalam data
+    df2[['Android Ver']] = playstore[['Android Ver']].astype('category')
+    df3 = df2.groupby(["Android Ver"]).count().sort_values('Reviews', ascending = False).head(5).reset_index()
+    X = df3['Android Ver']
+    Y = df3['App']
+    my_colors = 'rgbkymc'
+    # bagian ini digunakan untuk membuat kanvas/figure
+    fig = plt.figure(figsize=(8, 3), dpi=300)
+    fig.add_subplot()
+    # bagian ini digunakan untuk membuat bar plot
+    # isi variabel x dan y yang telah di definisikan di atas
+    plt.bar(X, Y, color=my_colors,)
+    # bagian ini digunakan untuk menyimpan plot dalam format image.png
+    plt.savefig('Version.png', bbox_inches="tight")
+
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue())
+    result4 = str(figdata_png)[2:-1]
 
     # Tambahkan hasil result plot pada fungsi render_template()
-    return render_template('index.html', stats=stats, result=result, result2=result2, result3=result3)
+    return render_template('index.html', stats=stats, result=result, result2=result2, result3=result3, result4=result4)
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     app.run(debug=True)
